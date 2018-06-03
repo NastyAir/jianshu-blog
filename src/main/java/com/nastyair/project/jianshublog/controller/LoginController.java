5a3eb7fa0f7baed833d494c7020d53e29cdd99b9
NastyAir <1115094858@qq.com>
2018-06-03 22:51:29 +0800

    禁用security

--- src/main/java/com/nastyair/project/jianshublog/controller/LoginController.java
+++ src/main/java/com/nastyair/project/jianshublog/controller/LoginController.java
@@ -1,4 +1,17 @@
 package com.nastyair.project.jianshublog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+    @RequestMapping("/login")
+    String login(
+            @RequestParam String username,
+            @RequestParam String password
+    ){
+        return "ok";
+    }
+
 }
